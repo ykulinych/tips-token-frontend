86a68db79e948f6b1583aa702614481d94dc96cd
Yan Kulinych <ykulinych@gmail.com>
2025-02-16 09:31:29 +0000

    init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return <>TipsToken</>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">TipsToken</h1>
+    </>
+  );
 };
 
 export default App;
