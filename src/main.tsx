0c232ce5d3bc9b38683b456cd7924784736036a2
Yan Kulinych <ykulinych@gmail.com>
2025-02-17 09:10:36 +0000

    init rtk

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./reset.css";
+import { Provider } from "react-redux";
+import store from "./store";
 import { SolanaProvider } from "./components/solana/SolanaProvider.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SolanaProvider>
-      <App />
-    </SolanaProvider>
+    <Provider store={store}>
+      <SolanaProvider>
+        <App />
+      </SolanaProvider>
+    </Provider>
   </StrictMode>
 );
